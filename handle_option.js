bc383872b089a45923144ee8ec584ad71e0e6197
sorzz <39261136+sorzz@users.noreply.github.com>
2021-07-22 04:30:53 +0900

    국가별 구현
    국가 선택도 적용됨

--- handle_option.js
+++ handle_option.js
@@ -1,31 +1,60 @@
 // 버튼 누르면 바로 실행되는 부분.
 // 연도별은 기본으로 출력
 
-// 사용자가 선택한 모든 것들을 정리해서 출력 함수를 호출해줄 것
+
+// 나라는 국가명 리스트랑 국가코드 리스트를 같이 넘겨줘야겠네..
 $("#display").click(function () { 
     // 1. 기준
-    let standard = $("input[name='standard']:checked").val(); // 장르별 연도별 뭐 이런거!
+    let standard = $("input[name='standard']:checked").val();  
 
     // 2. 범주 [시작년도, 끝년도, 국가리스트, 장르리스트]
     let start_year =  $("#start").val(); // 시작
     let end_year = $("#end").val(); // 끝
-    let country = $("select[name=set-country]").val();
+
+    let nNm = [];
+    let nCd = [];
+    if ($("input:checkbox[name=countryAll]").is(":checked") == true)  { 
+        nNm = '전체';
+    }
+    else {
+        const checkboxes = document.querySelectorAll('input[name="country"]');
+
+        checkboxes.forEach((checkbox) => {
+            if(checkbox.checked){
+                nNm.push(checkbox.id); // 이러면 이제 각 국가 이니셜 들어감.
+                nCd.push(checkbox.value); // 이러면 이제 각 국가 코드가 들어감.
+            }
+        })
+    }
 
     let sel_genre = [];
     if ($("input:checkbox[name=genreAll]").is(":checked") == true)  { // 전체에 체크가 되어있다면
         sel_genre.push('전체');
     }
+    else {
+        const checkboxes = document.querySelectorAll('input[name="genre"]');
 
+        checkboxes.forEach((checkbox) => {
+            if(checkbox.checked){
+                sel_genre.push(checkbox.value); // 이러면 이제 각 이름대로 들어감.
+            }
+        })
+    }
+
+    console.log(start_year,end_year,nNm,nCd,sel_genre);
     switch(standard){
         case '장르별':
-            genre(start_year,end_year,sel_genre);
+            genre(start_year,end_year,nCd,sel_genre); // 장르는 그냥 코드만 넘겨줘도 됨.
             break;
         case '국가별':
-            nation(start_year,end_year,sel_genre);
+            nation(start_year,end_year,nNm,nCd,sel_genre);
             break;
     }
 })
 
+$("#save_image").click(function () { 
+    test();
+})
 
 
 
@@ -63,5 +92,33 @@ function check(box)  {
         }
     })
 }
+
+
+
+// 장르에서 전체 누르면 다 체크 되고 나머지 누르면 전체 해제하기.
+function selectAll2(selectAll)  {
+    const checkboxes 
+        = document.querySelectorAll('input[name="country"]');
+    
+    checkboxes.forEach((checkbox) => {
+        checkbox.set
+        checkbox.checked = selectAll.checked
+    })
+}
+
+// 이제 다른거 누르면 전체 선택이 해제 되기. & 다 선택되면 전체도 체크되게 하기.
+function check2(box)  {
+    var selAll2 = document.getElementById('coun_all');
+    const checkboxes  = document.querySelectorAll('input[name="country"]');
+    
+    selAll2.checked = true; 
+
+    checkboxes.forEach((checkbox) => {
+        if(!checkbox.checked) {
+            selAll2.checked = checkbox.checked;
+        }
+    })
+}
+
 
 
--- parse_data.js
+++ parse_data.js
@@ -13,6 +13,7 @@ let info_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/search
 
 let label_str;
 let standards=[];
+let result;
 
 let first_time = true;
 let totalcnt;
@@ -20,119 +21,181 @@ var check_cnt=0;
 
 let alert_err = false;
 
-let nationNm = ['한국','미국','영국','인도','일본','중국'];
-let nationCd = ['22041011','22042002','22044010','22041007','22041008'];
+let def_nationNm = ['한국','미국','영국','일본','중국'];
+let def_nationCd = ['22041011','22042002','22044010','22041008','22041009'];
 
-function nation(start_year,end_year,sel_genre){ 
+let nationNm;
+let nationCd;
 
-  // arr[5][2]
-  var result = new Array(nationCd.length);
+
+let myLineChart;
+
+function nation(start_year,end_year,nNm,nCd,sel_genre){ 
+  if(nNm == '전체'){
+    nationNm = def_nationNm;
+    nationCd=def_nationCd;
+  }
+  else{
+    nationNm=nNm;
+    nationCd=nCd;
+  }
+  nationNm.push('전체');
+
+  console.log('국가별 시작이라구요~' + nationCd.length * (end_year-start_year+1));
+
+  // standards에 연도 넣기
+  for(let j=start_year; j<=end_year; j++) {
+
+    standards[j-start_year] = j;
+  }
+
+  // arr[국가 수+전체][연도 수]
+  result = new Array(nationCd.length+1);
 
   for (var i = 0; i < result.length; i++) {
     result[i] = new Array(end_year-start_year+1);
+    for (let j = 0; j <= end_year-start_year; j++) {
+      result[i][j]=0; // 0으로 초기화      
+    }
   }
+
 
   label_str = '연도별 영화 수';
   check_cnt=0;
   totalcnt=[];
+  console.log('국가별 중간이라구요~');
 
-  for(let i=0; i<nationCd.length; i++) {
-    for(let j=start_year; j<end_year+1; j++) {
+  for(let i=0; i<nationCd.length; i++) { // 각 나라 마다
+    console.log(nationNm[i]+'의 개수를 세보겟습니다.');
+
+    for(let j=start_year; j<=end_year; j++) { // 각 연도 마다
+       
       $.ajax({
         method: "GET",
         url: list_url+"&openStartDt="+ j +"&openEndDt="+j+"&repNationCd="+nationCd[i],
         dataType:"json",
         success:function(data) {
-            console.log(data);
-            check_cnt++;
-            console.log(j+"년 영화 수 : "+result[i-start_year]);
-            if(check_cnt == nationCd.length * (end_year-start_year+1)) {
-                draw(label_str,'line');
-            }
-            result[i][j] = data.movieListResult.totCnt;
-            
+          console.log(data);
+          result[i][j-start_year] = data.movieListResult.totCnt;
+          result[nationCd.length][j-start_year] += data.movieListResult.totCnt;
+          check_cnt++;
+          if(check_cnt == nationCd.length * (end_year-start_year+1)) {
+              draw(label_str,'line');
+          }
         }
       })
     }  
-    
   }
- 
- 
 }
 
 function genre(start_year,end_year,sel_genre) {
 }
 
+
+// 이거 hover했을때 2016 숫자 안뜨고 그냥 원소값만 뜨게 하기!!
 function draw(label_str,shape) {
     // 그래프로 표현 하기.
+    console.log("draw! is first?" + first_time);
+    my_datasets=[];
+
     if(first_time){
         first_time = false;
-        console.log(first_time);
     }
     else{
         myLineChart.destroy();
-        console.log(first_time);
     }
 
+    for(let i=0; i<=nationCd.length; i++){
+      my_datasets[i] = {
+        label: nationNm[i],
+        data: result[i],
+        borderColor: '#'+Math.round(Math.random()*0xffffff).toString(16),
+        yAxisID: 'y'}
+    }
+    
     myLineChart = new Chart(document.getElementById('line-chart').getContext('2d'),{
         type : shape,
         data :  {
           labels: standards,
-          datasets: [
-            {
-              label: 'Dataset 1',
-              data: result[0],
-              borderColor: 'red',
-              yAxisID: 'y',
-            },
-            {
-              label: 'Dataset 2',
-              data: result[1],
-              borderColor: 'blue',
-              yAxisID: 'y',
-            },
-            {
-              label: 'Dataset 2',
-              data: result[3],
-              borderColor: 'blue',
-              yAxisID: 'y',
-            },
-            {
-              label: 'Dataset 2',
-              data: result[4],
-              borderColor: 'blue',
-              yAxisID: 'y',
-            }
-          ]
+          datasets: my_datasets
         },
         options : {
             maintainAspectRatio : false, // 기본 비율 유지
             bezierCurve: true
+        },
+        tooltips:{
+          enabled:false
+        },
+        hover: {
+          animationDuration: 0
+        },
+        animation: {
+          duration: 1,
+          onComplete: function () {
+            var chartInstance = this.chart,
+              ctx = chartInstance.ctx;
+            ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+            ctx.fillStyle = 'purple';
+            ctx.textAlign = 'center';
+            ctx.textBaseline = 'bottom';
+  
+            this.data.datasets.forEach(function (dataset, i) {
+              var meta = chartInstance.controller.getDatasetMeta(i);
+              meta.data.forEach(function (bar, index) {
+                var data = dataset.data[index];							
+                ctx.fillText(data, bar._model.x, bar._model.y - 5);
+              });
+            });
+          }
         }
     });
 
     
 } 
   
-function draw2() {
-    const config = {
-        type: 'line',
-        data: data,
-        options: {
-          responsive: true,
-          plugins: {
-            legend: {
-              position: 'top',
-            },
-            title: {
-              display: true,
-              text: 'Chart.js Line Chart'
-            }
-          }
-        },
-      };
+function test() {
+  if(first_time){
+    first_time = false;
+  }
+  else{
+      myLineChart.destroy();
+  }
+  nations=['한국', '미국','영국'];
+  standards=[2010,2011,2012];
+  // arr[국가 수][연도 수]
+  var result = new Array(3);
 
+  for (var i = 0; i < 3; i++) {
+    result[i] = new Array(3);
+  }  
+  
+  result[0] = [10,15,18]; //한국 영화 수  
+  result[1] = [12,25,13]; //미국 영화 수  
+  result[2] = [20,17,11]; //영국 영화 수  
+  let my_datasets=[];
+
+  for(let i=0; i<3; i++){
+    my_datasets[i] = {
+      label: nations[i],
+      data: result[i],
+      borderColor: '#'+Math.round(Math.random()*0xffffff).toString(16),
+      yAxisID: 'y'}
+  }
+
+
+  yLineChart = new Chart(document.getElementById('line-chart').getContext('2d'),{
+    type : 'line',
+    data :  {
+      labels: standards,
+      datasets: my_datasets
+    },
+    options : {
+        maintainAspectRatio : false, // 기본 비율 유지
+        bezierCurve: true
+    }
+  });
 }
+
 // 그래프 모양 바뀔 때마다 바꿔주기
 function shape(nothing) {
     if (first_time){
