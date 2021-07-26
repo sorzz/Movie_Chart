25f492bf8769fcc80d9b56648bbcd392ad7489d8
sorzz <qkrthfwl8@naver.com>
2021-07-27 03:40:37 +0900

    유형 완료. 막대 선 원 따로 구현해야됨 이제.

--- handle_option.js
+++ handle_option.js
@@ -1,6 +1,18 @@
 // 버튼 누르면 바로 실행되는 부분.
 // 연도별은 기본으로 출력
 
+$(document).ready(function() {
+    $("#sel_genre").find(':input').prop('disabled', true);
+    $('#sel_genre a').click(function(e) {
+        e.preventDefault();
+    })
+    $("#sel_type").find(':input').prop('disabled', true);
+    $('#sel_type a').click(function(e) {
+        e.preventDefault();
+    })
+
+    
+})
 
 // 나라는 국가명 리스트랑 국가코드 리스트를 같이 넘겨줘야겠네..
 $("#display").click(function () { 
@@ -15,7 +27,8 @@ $("#display").click(function () {
     let nNm = [];
     let nCd = [];
     if ($("input:checkbox[name=countryAll]").is(":checked") == true)  { 
-        nNm = '전체';
+        nNm = ['한국', '미국', '영국', '일본', '중국', '전체'];
+        nCd = ['22041011', '22042002', '22044010', '22041008', '22041009'];
     }
     else {
         const checkboxes = document.querySelectorAll('input[name="country"]');
@@ -26,11 +39,13 @@ $("#display").click(function () {
                 nCd.push(checkbox.value); // 이러면 이제 각 국가 코드가 들어감.
             }
         })
+        nNm.push('전체'); // 이러면 이제 각 국가 이니셜 들어감.
+
     }
 
     let sel_genre = [];
     if ($("input:checkbox[name=genreAll]").is(":checked") == true)  { // 전체에 체크가 되어있다면
-        sel_genre.push('전체');
+        sel_genre = ['애니메이션', '스릴러', '코미디', 'SF', '드라마', '전체'];
     }
     else {
         const checkboxes = document.querySelectorAll('input[name="genre"]');
@@ -40,15 +55,38 @@ $("#display").click(function () {
                 sel_genre.push(checkbox.value); // 이러면 이제 각 이름대로 들어감.
             }
         })
+        sel_genre.push('전체');
     }
 
-    console.log(start_year,end_year,nNm,nCd,sel_genre);
+    let typeNm = [];
+    let typeCd = [];
+
+    if ($("input:checkbox[name=typeAll]").is(":checked") == true)  { // 전체에 체크가 되어있다면
+        typeNm=['장편','단편','옴니버스','전체'];
+        typeCd=['220101','220102','220103'];
+    }
+    else {
+        const checkboxes = document.querySelectorAll('input[name="type"]');
+
+        checkboxes.forEach((checkbox) => {
+            if(checkbox.checked){
+                typeNm.push(checkbox.id); // 이러면 이제 각 이름대로 들어감.
+                typeCd.push(checkbox.value)
+            }
+        })
+        typeNm.push('전체'); // 이러면 이제 각 국가 이니셜 들어감.
+    }
+
+    console.log(start_year,end_year,nNm,nCd,sel_genre,typeNm);
     switch(standard){
         case '장르별':
             genre(start_year,end_year,sel_genre); // 장르는 그냥 코드만 넘겨줘도 됨.
             break;
         case '국가별':
             nation(start_year,end_year,nNm,nCd);
+            break;
+        case '유형별':
+            type(start_year,end_year,typeNm,typeCd);
             break;
     }
 })
@@ -103,6 +141,17 @@ function selectAll2(selectAll)  {
     })
 }
 
+// 유형에서 전체 누르면 다 체크 되고 나머지 누르면 전체 해제하기.
+function selectAll3(selectAll)  {
+    const checkboxes 
+        = document.querySelectorAll('input[name="type"]');
+    
+    checkboxes.forEach((checkbox) => {
+        checkbox.set
+        checkbox.checked = selectAll.checked
+    })
+}
+
 // 이제 다른거 누르면 전체 선택이 해제 되기. & 다 선택되면 전체도 체크되게 하기.
 function check2(box)  {
     var selAll2 = document.getElementById('coun_all');
@@ -117,6 +166,21 @@ function check2(box)  {
     })
 }
 
+// 이제 다른거 누르면 전체 선택이 해제 되기. & 다 선택되면 전체도 체크되게 하기.
+function check3(box)  {
+    var selAll = document.getElementById('type_all');
+    const checkboxes  = document.querySelectorAll('input[name="type"]');
+    
+    selAll.checked = true; 
+
+    checkboxes.forEach((checkbox) => {
+        if(!checkbox.checked) {
+            selAll.checked = checkbox.checked;
+        }
+    })
+}
+
+
 $('#set-standard').click(function () {
     var radioVal = $('input[name="standard"]:checked').val();
     if (radioVal == '국가별') 
@@ -125,18 +189,39 @@ $('#set-standard').click(function () {
         $('#sel_genre a').click(function(e) {
             e.preventDefault();
         })
+        $("#sel_type").find(':input').prop('disabled', true);
+        $('#sel_type a').click(function(e) {
+            e.preventDefault();
+        })
 
         $('#sel_country').find(':input').prop('disabled', false);
         $('#sel_country a').unbind("click");
+    }
+    else if (radioVal == '장르별'){
+        $("#sel_country").find(':input').prop('disabled', true);
+        $('#sel_country a').click(function(e) {
+            e.preventDefault();
+        })
+        $("#sel_type").find(':input').prop('disabled', true);
+        $('#sel_type a').click(function(e) {
+            e.preventDefault();
+        })
+
+        $('#sel_genre').find(':input').prop('disabled', false);
+        $('#sel_genre a').unbind("click");
     }
     else {
         $("#sel_country").find(':input').prop('disabled', true);
         $('#sel_country a').click(function(e) {
             e.preventDefault();
         })
+        $("#sel_genre").find(':input').prop('disabled', true);
+        $('#sel_genre a').click(function(e) {
+            e.preventDefault();
+        })
 
-        $('#sel_genre').find(':input').prop('disabled', false);
-        $('#sel_genre a').unbind("click");
+        $('#sel_type').find(':input').prop('disabled', false);
+        $('#sel_type a').unbind("click");
     }
 });
 
